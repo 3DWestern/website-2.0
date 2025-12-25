cfcb29954f9a58d6d9d5d9ff724b9252d15c7653
Thomson Lam <thomson.lam327@gmail.com>
2025-12-25 17:48:37 -0500

    updated root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,30 +3,76 @@ import "./globals.css";
 import { LoadingWrapper } from "@/components/LoadingWrapper";
 import { HomeButton } from "@/components/HomeButton";
 
+// global metadata for SEO, can be overridden by specific individual pages
 export const metadata: Metadata = {
-  title: "3DW Makerspace",
-  description: "A student-run organization partnering with Morrissette Entrepreneurship to manage Western's makerspaces. From idea validation to product creation.",
-  icons: {
-    icon: "/logo.png",
-  },
+	title: "3DW Makerspace",
+	description: "A student-run organization partnering with Morrissette Entrepreneurship to manage Western's makerspaces. From idea validation to product creation.",
+	keywords: ["3D Printing", "Western University", "3D Western", "Engineering", "Western Printing Club"],
+	publisher: "3D Western",
+	openGraph: {
+		title: "3D Western",
+		description: "Western University's Official 3D Printing Club",
+		url: "https://3dwestern.ca",
+		siteName: "3D Western",
+		images: [
+			{
+				url: "https://3dwestern.ca/banner.png",
+				width: 1200,
+				height: 630,
+				alt: "3D Western: Western University's Official 3D Printing Club",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	icons: {
+		icon: '/logo.png',
+		shortcut: '/favicon.ico', // TODO: replace with new logo.png if needed; otherwise stick to favicon
+		apple: '/favicon.ico',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: false,
+		},
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "3D Western",
+		description: "Western University's Official 3D Printing Club",
+		images: [
+			{
+				url: "https://3dwestern.ca/banner.png",
+				width: 1200,
+				height: 630,
+				alt: "3D Western: Western University's Official 3D Printing Club",
+			},
+		],
+		//	creator: "@yourTwitterHandle", // optional
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  // Use a client component to access the router
-  return (
-    <html lang="en" className="scroll-smooth">
-      <body className={`antialiased`}>
-        <HomeButton />
-        <LoadingWrapper>
-          {children}
-        </LoadingWrapper>
-      </body>
-    </html>
-  );
+	// Use a client component to access the router
+	return (
+		<html lang="en" className="scroll-smooth">
+			<body className={`antialiased`}>
+				<HomeButton />
+				<LoadingWrapper>
+					{children}
+				</LoadingWrapper>
+			</body>
+		</html>
+	);
 }
 
 // HomeButton is now a client component in components/HomeButton.tsx
+
