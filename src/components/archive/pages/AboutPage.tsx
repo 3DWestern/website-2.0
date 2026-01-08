0b7dcd8dd7187d8cee581f4a44b94b031235d9ab
Thomson Lam <thomson.lam327@gmail.com>
2026-01-07 22:02:56 -0500

    removed lint warning for unused about page

--- src/components/archive/pages/AboutPage.tsx
+++ src/components/archive/pages/AboutPage.tsx
@@ -43,11 +43,11 @@ export function AboutPage() {
 							</div>
 						</div>
 						<div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-2xl">
-							<img
+							{/* NOTE: commented out	<img
 								src="https://images.unsplash.com/photo-1758876203342-fc14c0bba67c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx0ZWFtJTIwY29sbGFib3JhdGlvbiUyMG9mZmljZXxlbnwxfHx8fDE3NjMyODcxNzJ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
 								alt="Team collaboration"
 								className="w-full h-full object-cover"
-							/>
+							/> */}
 						</div>
 					</div>
 				</div>
