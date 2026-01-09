1514cbddb36ee0d235bd7e5240e9c6dfb6e035d9
Thomson Lam <thomson.lam327@gmail.com>
2026-01-08 22:03:39 -0500

    edited mobile width for big text

--- src/components/sections/BigTextSection.tsx
+++ src/components/sections/BigTextSection.tsx
@@ -4,9 +4,9 @@ import { koulen } from "@/lib/fonts";
 export default function BigTextSection() {
 	return (
 		<section className="flex w-screen min-h-screen flex-col items-center text-center py-20 gap-y-20">
-			<div className="mx-auto flex flex-col items-center justify-center gap-y-20">
+			<div className="mx-auto flex flex-col items-center justify-center gap-y-10 sm:gap-y-20">
 				<h1 className={`text-5xl sm:text-7xl md:text-9xl ${koulen.className}`}>3D WESTERN</h1>
-				<div className="font-bold text-black/50 p-4 w-4/6 mx-auto text-center sm:text-left flex flex-col items-start justify-center space-y-6 md:space-y-12 ">
+				<div className="font-bold text-black/50 p-4 w-5/6 sm:w-4/6 mx-auto text-center sm:text-left flex flex-col items-start justify-center space-y-6 md:space-y-12 ">
 					<p className="text-lg sm:text-xl md:text-2xl">{bigstats.description}</p>
 					<p className="text-lg sm:text-xl md:text-2xl">{bigstats.details}</p>
 				</div>
