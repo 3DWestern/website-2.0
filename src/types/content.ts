77c1bb73106f6a32425be77b34d1d16413310a24
Sam Deitz <deitsamm905@gmail.com>
2026-06-07 13:09:23 -0400

    types

---
+++ src/types/content.ts
@@ -0,0 +1,17 @@
+export type BlogPost = {
+  id: number;
+  title: string;
+  author: string;
+  image: string;
+  alt: string;
+  date: string;
+  tags: string[];
+};
+
+export type Showcase = {
+  id: number;
+  title: string;
+  creator: string;
+  image: string;
+  alt: string;
+};
