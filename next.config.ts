962c435522c60c88cff4a5c33d7aab40f8c2be77
Kay Roye <05kalusss@gmail.com>
2026-08-16 15:26:51 -0400

    Update next config to handle test images from crux CDN

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+      remotePatterns: [new URL("https://www.example.com/**"), new URL("https://cdn.cruxplanner.ca/**")],
+  }
 };
 
 export default withPayload(nextConfig);
